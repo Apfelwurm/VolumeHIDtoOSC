e2eb37e6e66c14d59e6607cef3d19367128e0bf5
Alexander Volz <Alexander@volzit.de>
2021-09-08 01:43:32 +0200

    fix build script arm

--- rebuild_armhf.sh
+++ rebuild_armhf.sh
@@ -7,7 +7,7 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr ../
 make
 mkdir packageroot
 cp -rf ../DEBIANARMHF packageroot/
-mv DEBIANARMHF DEBIAN
+mv packageroot/DEBIANARMHF packageroot/DEBIAN
 mkdir -p packageroot/usr/bin/
 mkdir -p packageroot/etc/volumehidtoosc/
 mkdir -p packageroot/usr/lib/systemd/system/
