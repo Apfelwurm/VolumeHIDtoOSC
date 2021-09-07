24c897196f4629c67db2e27fcaf71cb146b6a133
Alexander Volz <Alexander@volzit.de>
2021-09-08 01:41:15 +0200

    added armhf buildscript

---
+++ rebuild_armhf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm -rf build
+set -e
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr ../ 
+make
+mkdir packageroot
+cp -rf ../DEBIANARMHF packageroot/
+mkdir -p packageroot/usr/bin/
+mkdir -p packageroot/etc/volumehidtoosc/
+mkdir -p packageroot/usr/lib/systemd/system/
+mkdir -p packageroot/var/log/volumehidtoosc/
+
+cp bin/volumehidtoosc packageroot/usr/bin/
+cp ../volumehidtoosc.conf packageroot/etc/volumehidtoosc/
+cp ../volumehidtoosc.service packageroot/usr/lib/systemd/system/
+touch packageroot/var/log/volumehidtoosc/volumehidtoosc.log
+
+dpkg-deb -b packageroot volumehidtoosc_1.0.0_armhf.deb
+
+
+
